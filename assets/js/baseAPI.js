892eae2075fb37d1c85b4aa327b811d85c2383a8
ypy <1160238302@qq.com>
2022-08-04 11:19:07 +0800

    index0804

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,23 @@ const baseUrl = "http://www.liulongbin.top:3007";
 
 
 $.ajaxPrefilter((option) => {
+    if (option.url.includes("/my/")) {
+        option.headers = {
+            Authorization: localStorage.getItem("token"),
+        };
+    }
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = baseUrl + option.url;
+
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    option.complete = (res) => {
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //  强制清空 token
+            localStorage.removeItem("token");
+            // 强制跳转到登录页面
+            location.href = "/login.html"
+        }
+    }
 });
+
